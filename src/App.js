b0a9ae1779e0bb7c3886c443660c06c2d702e221
FredericGuerin <fguerin555@gmail.com>
2025-04-17 18:49:41 +0200

    jeudi17avril18h49

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./Global.css";
+import Booking from "./pages/BookingFile/Booking";
+import ReservationList from "./pages/ReservationListFile/ReservationList";
+import DaybyDay from "./pages/DaybyDayFile/DaybyDay";
+import CodBarreReader from "./pages/CodBarreReaderFile/CodBarreReader";
+import BeachPlanPeriod from "./pages/BeachPlanPeriodFile/BeachPlanPeriod";
+import BeachPlanMorning from "./pages/BeachPlanMorningFile/BeachPlanMorning";
+import BeachPlanAfternoon from "./pages/BeachPlanAfternoonFile/BeachPlanAfternoon";
+import Navbar from "./components/Navbar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/Parasols" element={<Booking />} />
+          <Route path="/" element={<Booking />} />
+          <Route path="/ReservationList" element={<ReservationList />} />
 
+          <Route path="/daybyday" element={<DaybyDay />} />
+          <Route path="/codbarrereader" element={<CodBarreReader />} />
+          <Route path="/beachplanperiod" element={<BeachPlanPeriod />} />
+          <Route path="/daybyday" element={<DaybyDay />} />
+          <Route path="/beachplanmorning" element={<BeachPlanMorning />} />
+          <Route path="/beachplanafternoon" element={<BeachPlanAfternoon />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 export default App;
