b6d0b59bebdaec667309a3f29ce5f3fd2978dbf8
FredericGuerin <fguerin555@gmail.com>
2025-04-18 18:08:50 +0200

    18avril18h08

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+
+ # rendre le fichier executable (normalement une fois suffit) : chmod +x deploy.sh
+ # lancer le script : ./deploy.sh
+
+ #!/bin/bash
+
 echo "Choisir la méthode de déploiement :"
 echo "1) GitHub Pages"
 echo "2) Vercel"
@@ -7,13 +14,12 @@ read -p "Entrez 1 ou 2 : " choice
 
 if [ "$choice" == "1" ]; then
   echo "Déploiement sur GitHub Pages..."
+  npm install  # Installe les dépendances
   npm run deploy  # Assurez-vous que le script "deploy" est correctement configuré dans votre package.json
 elif [ "$choice" == "2" ]; then
   echo "Déploiement sur Vercel..."
+  npm install  # Installe les dépendances
   vercel --prod
 else
   echo "Choix invalide. Veuillez entrer 1 ou 2."
 fi
-
- # rendre le fichier executable (normalement une fois suffit) : chmod +x deploy.sh
- # lancer le script : ./deploy.sh
