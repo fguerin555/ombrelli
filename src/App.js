3c2922ce321091040cbe084f7c2bd1097cf47f65
FredericGuerin <fguerin555@gmail.com>
2025-05-06 20:39:37 +0200

    router modifié

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BeachPlan from "./pages/BeachPlanFile/BeachPlan";
 import ReservationList from "./pages/ReservationListFile/ReservationList";
 import Query from "./pages/QueryFile/Query";
 import QRCodeReader from "./pages/QRCodeReaderFile/QRCodeReader";
+import QRCodeHandlerPage from "./pages/QRCodeHandlerPageFile/QRCodeHandlerPage";
 import BeachPlanPeriod from "./pages/BeachPlanPeriodFile/BeachPlanPeriod";
 import ChangeExchange from "./pages/ChangeExchangeFile/ChangeExchange";
 import TestQueryName from "./TestQueryName";
@@ -13,20 +14,22 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/ombrelli">
       <div>
         <Navbar />
         <Routes>
           <Route path="/ombrelli" element={<BeachPlan />} />
           <Route path="/" element={<BeachPlan />} />
           <Route path="/beachplan" element={<BeachPlan />} />
-          <Route path="/ReservationList" element={<ReservationList />} />
+          <Route path="/reservationList" element={<ReservationList />} />
           <Route path="/changeexchange" element={<ChangeExchange />} />
           <Route path="/qrcodereader" element={<QRCodeReader />} />
           <Route path="/beachplanperiod" element={<BeachPlanPeriod />} />
           <Route path="/query" element={<Query />} />
           <Route path="/testqueryplan" element={<TestQueryPlan />} />
           <Route path="/testqueryname" element={<TestQueryName />} />
+
+          <Route path="/qr-scan-resultat" element={<QRCodeHandlerPage />} />
         </Routes>
       </div>
     </Router>
