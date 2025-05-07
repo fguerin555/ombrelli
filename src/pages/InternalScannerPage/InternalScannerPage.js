3e262eed3838c90880d4493df8017721c5124d53
FredericGuerin <fguerin555@gmail.com>
2025-05-07 17:28:45 +0200

    testrescanpause

--- src/pages/InternalScannerPage/InternalScannerPage.js
+++ src/pages/InternalScannerPage/InternalScannerPage.js
@@ -7,9 +7,12 @@ const InternalScannerPage = () => {
   const [scannedCode, setScannedCode] = useState(null);
   const [showScanner, setShowScanner] = useState(true); // Afficher le scanner par défaut
   const [scannerKey, setScannerKey] = useState(Date.now()); // Nouvel état pour la clé
+  const [isScannerPaused, setIsScannerPaused] = useState(false);
   const [error, setError] = useState("");
 
   const { ref } = useZxing({
+    paused: isScannerPaused, // Utiliser l'état pour contrôler la pause
+
     onDecodeResult: (result) => {
       let codeValue = result.getText();
       console.log("Code brut scanné en interne:", codeValue);
@@ -37,6 +40,7 @@ const InternalScannerPage = () => {
 
       setScannedCode(codeValue);
       setShowScanner(false); // Masquer le scanner après un scan réussi
+      setIsScannerPaused(true); // Mettre en pause après un scan réussi
       setError(""); // Effacer les erreurs précédentes en cas de succès
     },
     onDecodeError: (decodeError) => {
@@ -67,6 +71,7 @@ const InternalScannerPage = () => {
     setScannedCode(null);
     setError("");
     setShowScanner(true); // Important: réactiver le scanner
+    setIsScannerPaused(false); // Réactiver le scanner
     setScannerKey(Date.now()); // Changer la clé pour forcer le remontage de la vidéo
   };
 
